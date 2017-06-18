e4a1f99a3b948f64d9538838f1122c5a2c74a03e
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-06-18 03:06:31 -0700

    Driver class to test!

---
+++ Java02-Lab02/Lab02-a/src/Driver.java
@@ -0,0 +1,46 @@
+/**
+ * @author KateYeEumLee
+ * @version 2.1
+ */
+public class Driver {
+
+	/**
+	 * 
+	 */
+	public Driver() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Driver().run();
+	}
+
+	public void run(){
+		StudentDatabase database = new StudentDatabase();
+		
+		//student added in the database (HashMap).
+		database.addStudent("KATE");
+		database.addStudent("TIMO");
+		database.addStudent("bean");
+		
+		//changed quiz mark for each student.
+		database.changeQuizMark("KATE", 2, 80);
+		database.changeQuizMark("TIMO", 6, 90);
+		database.changeQuizMark("bean", 8, 50);
+		
+		//get quiz of just bean student.
+		System.out.println("The quiz mark of student, Bean :");
+		System.out.println(database.getQuizzes("BEAN"));
+		System.out.println("");
+		
+		//display all makrs of all students in database.
+		System.out.println("Display all quiz marks of all students : ");
+		database.showQuizMarks();
+		System.out.println("");
+		
+	}
+}
