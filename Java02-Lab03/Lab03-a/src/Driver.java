df725a22b6c67292a930f09a4d0505aa67dc94e6
yiyeum <KateYeEumLee@iyeeums-air.wifi.bcit.ca>
2017-06-20 15:48:27 -0700

    Driver class to test methods

---
+++ Java02-Lab03/Lab03-a/src/Driver.java
@@ -0,0 +1,52 @@
+/**
+ * @author KateYeEumLee
+ * @version 3.1
+ */
+public class Driver {
+	private Refrigerator refrige;
+	private Stove stove;
+	private ApplianceStore store;
+	
+	/**
+	 * Constructor 
+	 */
+	public Driver() {
+		// TODO Auto-generated constructor stub
+		refrige = new Refrigerator("Samsung","RG-100",30,50,80,20);
+		stove = new Stove("LG","ST-100",20,40,50,7);
+		store = new ApplianceStore();
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Driver().run();
+
+	}
+
+	public void run(){
+		
+		//Test methods in ApplianceStore class.
+		//add refrige and stove to the appliance store(arrayList)
+		store.addAppliances(refrige);
+		store.addAppliances(stove);
+		
+		System.out.println("The number of appliances in the store :");
+		System.out.println(store.countOfAppliances());
+		System.out.println("");
+		
+		System.out.println("Display the detail of appliances : ");
+		store.displayAppliance();
+		System.out.println("");
+		
+		//Test each of appliances
+		System.out.println("The capacity of refrigerator :");
+		System.out.println(refrige.calculateCapacity());
+		System.out.println("");
+		
+		
+	}
+	
+}
