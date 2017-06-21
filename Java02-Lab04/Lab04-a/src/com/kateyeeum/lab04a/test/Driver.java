234e39506bf1b1c136b69a46c5d7536dea06265b
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-06-20 22:09:26 -0700

    Driver class to test methods

---
+++ Java02-Lab04/Lab04-a/src/com/kateyeeum/lab04a/test/Driver.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.kateyeeum.lab04a.test;
+
+import com.kateyeeum.lab04a.data.Cat;
+import com.kateyeeum.lab04a.data.Dog;
+import com.kateyeeum.lab04a.database.AnimalHotel;
+
+/**
+ * @author KateYeEumLee
+ * @version 4.1
+ */
+public class Driver {
+	private Dog myDog;
+	private Cat myCat;
+	private AnimalHotel hotel;
+	
+	/**
+	 * Constructor
+	 */
+	public Driver() {
+		// TODO Auto-generated constructor stub
+		myDog = new Dog("Puddle" , 6.0 , true);
+		myCat = new Cat("Kitty", 5.0 , false);
+		hotel = new AnimalHotel();
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Driver().run();
+	}
+
+	public void run(){
+		//add new animals to hotel
+		hotel.addNewAnimals(myCat);
+		hotel.addNewAnimals(myDog);
+		
+		//display detail of all animals in the hotel.
+		System.out.println("");
+		System.out.println("Display all animals members in the hotel : ");
+		hotel.displayDetailOfAllAnimals();
+		
+	}
+}
