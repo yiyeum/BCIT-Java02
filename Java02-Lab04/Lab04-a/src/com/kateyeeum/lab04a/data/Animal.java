8477c101d75d8e919194704f0ce8db56c36318f5
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-06-20 21:45:50 -0700

    Animal - Super class created
    -instance variables
    -two constructors
    -getters / setters
    -toString for instance variables

---
+++ Java02-Lab04/Lab04-a/src/com/kateyeeum/lab04a/data/Animal.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+package com.kateyeeum.lab04a.data;
+
+/**
+ * @author KateYeEumLee
+ * @version 4.1
+ */
+public class Animal {
+	private String breed;
+	private double weight;
+	
+	/**
+	 * Default Constructor
+	 */
+	public Animal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	/**
+	 * Overloaded Constructor
+	 * @param breed The breed of animal as String.
+	 * @param weight The weight of animal as double.
+	 */
+	public Animal(String breed, double weight) {
+		setBreed(breed);
+		setWeight(weight);
+	}
+
+
+	/**
+	 * Method to get the breed of animal.
+	 * @return the breed as String.
+	 */
+	public String getBreed() {
+		return breed;
+	}
+
+	/**
+	 * Method to set the breed of animal.
+	 * @param breed the breed to set
+	 */
+	public void setBreed(String breed) {
+		if(breed != null && breed.trim().length() > 0){
+			this.breed = breed;
+		}
+	}
+
+	/**
+	 * Method to get the weight of animal.
+	 * @return the weight as double.
+	 */
+	public double getWeight() {
+		return weight;
+	}
+
+	/**
+	 * Method to set the weight of animal.
+	 * @param weight the weight to set
+	 */
+	public void setWeight(double weight) {
+		if(weight > 0.0){
+			this.weight = weight;
+		}
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Animal [breed=" + breed + ", weight=" + weight + "]";
+	}
+	
+
+}
