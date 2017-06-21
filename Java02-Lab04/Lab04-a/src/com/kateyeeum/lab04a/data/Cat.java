d8a7861bf59597dff9795fd2d9d5920f9228dac4
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-06-20 21:49:20 -0700

    Cat class - sub class created
    -instance variable
    -constructors
    -getter / setter
    -toString

---
+++ Java02-Lab04/Lab04-a/src/com/kateyeeum/lab04a/data/Cat.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package com.kateyeeum.lab04a.data;
+
+/**
+ * @author KateYeEumLee
+ * @version 4.1
+ */
+public class Cat extends Animal {
+	private boolean hunts;
+	
+	/**
+	 * Default Constructor
+	 */
+	public Cat() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+
+	/**
+	 * Overloaded Constructor
+	 * @param breed The breed of animal as String.
+	 * @param weight The weight of animal as double.
+	 * @param hunts 
+	 */
+	public Cat(String breed, double weight, boolean hunts) {
+		super(breed, weight);
+		setHunts(hunts);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Method to check if the cat hunts
+	 * @return the hunts as boolean.
+	 */
+	public boolean isHunts() {
+		return hunts;
+	}
+
+	/**
+	 * Method to set if the cat hunts.
+	 * @param hunts the hunts to set
+	 */
+	public void setHunts(boolean hunts) {
+		this.hunts = hunts;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Cat [hunts=" + hunts + ", toString()=" + super.toString() + "]";
+	}
+
+	
+}
