8f0abaf429e5ccd2324e1b1d97b9c4425cda880d
yiyeum <KateYeEumLee@iyeeums-air.wifi.bcit.ca>
2017-06-23 13:01:02 -0700

    Driver class to test method.

---
+++ Java02-Lab05/Lab05-a/src/com/kateyeeum/lab05a/test/Driver.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.kateyeeum.lab05a.test;
+
+import com.kateyeeum.lab05a.data.CommissionEmployee;
+import com.kateyeeum.lab05a.data.HourlyEmployee;
+
+/**
+ * @author KateYeEumLee
+ * @version 5.1
+ */
+public class Driver {
+	private CommissionEmployee commissionEmployee;
+	private HourlyEmployee hourlyEmployee;
+	
+	/**
+	 * Constructor
+	 */
+	public Driver() {
+		// TODO Auto-generated constructor stub
+		commissionEmployee  = new CommissionEmployee("Katie" , 3.0 , 1500);
+		hourlyEmployee = new HourlyEmployee("Timothy" , 40.5);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Driver().run();
+
+	}
+	
+	public void run(){
+		System.out.println("The monthly earning of commission employee : " + commissionEmployee.calculateMonthlyEarnings());
+		System.out.println("The monthly earning of hourly employee : " + hourlyEmployee.calculateMonthlyEarnings());
+	}
+
+}
